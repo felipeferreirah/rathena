73b6a9981d7b16f35de20fb10b99f8f05350c690
Felipe Cabo <felipe.cabo@ibm.com>
2025-10-29 14:55:18 -0300

    style(login): remove trailing whitespace and clean up comments

--- src/login/account.cpp
+++ src/login/account.cpp
@@ -29,7 +29,7 @@ typedef struct AccountDB_SQL {
 	std::string db_password = "";
 	std::string db_database = "ragnarok";
 	std::string codepage = "";
-	// other settings
+	
 	bool case_sensitive;
 	//table name
 	char account_db[32];
